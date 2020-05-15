7bc6e2e8721cbb6ab202fd33a55ec3045cf26b3b
user <user@RUTO1230>
2020-05-15 12:17:38 +0900

    したくない

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -1,19 +1,15 @@
 
 public class GitTest {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public static void main(String[] args)
+	{
 		String melt="メルトを許せ";
 		if(melt=="メルとを許せ")
 		{
-			melt="メルトを許すな";
+			melt="メルトを許したくない";
 		}
 
-
 		System.out.println(melt);
-
-
 	}
 
 }
